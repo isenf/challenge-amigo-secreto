42a51a043b4af045c8037d2b0e52f521317fa60e
Fabro <fabromariaclara@gmail.com>
2025-01-24 20:30:51 -0300

    feat: remover amigo da lista

--- js/script.js
+++ js/script.js
@@ -14,29 +14,36 @@ document.addEventListener('DOMContentLoaded', ()=>{
         sortearAmigo(listaDeAmigos, elementoLista);
     });
 
+    atualizaStatusBtnSortear(listaDeAmigos, btnSortear);
+
 });
 
-function adicionarAmigo(input, lista, botao, elemento){
+function adicionarAmigo(input, lista, botaoSortear, elemento){
     if(input.value == ''){
-        alert("Você precisa digitar o nome do amigo para adicioná-lo!");
-        botao.setAttribute('disabled', true);
+        mostrarMensagem("Você precisa digitar o nome do amigo para adicioná-lo!");
+        botaoSortear.setAttribute('disabled', true);
+        
         return;
     }
     if(verificaLista(lista, input.value)){
-        alert(`${input.value} já está na lista de amigos!`);
+        mostrarMensagem(`${input.value} já está na lista de amigos!`);
         return;
     }
 
     lista.push(input.value);
-    atualizaListaAmigos(input.value, elemento);
-    botao.removeAttribute('disabled');
+    atualizaListaAmigos(input.value, lista, elemento, botaoSortear);
+    atualizaStatusBtnSortear(lista, botaoSortear);
     input.value = ''
 }
 
-function atualizaListaAmigos(amigo, elemento){
-    var li = document.createElement('li');
+function atualizaListaAmigos(amigo, lista, elemento, botaoSortear){
+    const li = document.createElement('li');
     li.textContent = amigo;
     elemento.appendChild(li);
+
+    const botaoRemover = criaBotaoRemover(amigo, lista, li, botaoSortear);
+
+    li.appendChild(botaoRemover);
 }
 
 function gerarSorteado(lista){
@@ -54,4 +61,35 @@ function sortearAmigo(lista, elemento){
 
 function verificaLista(lista, amigo){
     return lista.includes(amigo);
+}
+
+function mostrarMensagem(mensagem){
+    alert(mensagem)
+}
+
+function removerAmigo(amigo, lista, elementoLi, botaoSortear){
+    const indice = lista.indexOf(amigo);
+    if(indice !== -1){
+        lista.splice(indice, 1);
+    }
+
+    elementoLi.remove();
+    atualizaStatusBtnSortear(lista, botaoSortear);
+}
+
+function criaBotaoRemover(amigo, lista, li, botaoSortear){
+    const botaoRemover = document.createElement('img');
+    botaoRemover.src = './assets/lixeira.png';
+    botaoRemover.alt = 'Remover';
+    botaoRemover.style.width = '20px';
+
+    botaoRemover.addEventListener('click', ()=>{
+        removerAmigo(amigo, lista, li, botaoSortear)
+    })
+    return botaoRemover
+
+}
+
+function atualizaStatusBtnSortear(lista, botaoSortear){
+    lista.length === 0 ? botaoSortear.setAttribute('disabled', true) : botaoSortear.removeAttribute('disabled');
 }
